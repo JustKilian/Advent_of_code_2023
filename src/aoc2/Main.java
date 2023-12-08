4c498121adb272cf25e09d01eb60dd192b5c12a9
Kilian Achenbach <kilian.achenbach@ruhr-uni-bochum.de>
2023-12-09 00:50:47 +0100

    Day 2 part 1 finished

--- src/aoc2/Main.java
+++ src/aoc2/Main.java
@@ -17,24 +17,21 @@ public class Main {
             if (analyze(map, max) == null) {
                 continue;
             }
-            numberOfMatches += getNumberOfMatches(map.values().iterator().next());
+            numberOfMatches += Integer.parseInt(map.keySet().toArray()[0].toString());
         }
         WriteFile wf = new WriteFile("wf");
         wf.write(args[1], numberOfMatches.toString(), true);
     }
 
     public static Integer getNumberOfMatches(ArrayList<Map> matches){
-        System.out.println(matches);
         return matches.size();
     }
 
     public static Map<Integer, ArrayList<Map>> analyze(Map<Integer, ArrayList<Map>> map, ArrayList<Integer> max) {
-        Map<Integer, ArrayList<Map>> wrongoutput = new HashMap<Integer, ArrayList<Map>>();
-        Map<Integer, ArrayList<Map>> output = map;
+        Map<Integer, ArrayList<Map>> wrongoutput = new HashMap<Integer, ArrayList<Map>>(); // {1: [{"red": 4, "blue": 3}, {"red": 1, "green": 2, "blue": 6}, {"green": 2}]}
         one:
-        for (ArrayList<Map> entry : map.values()) {
-            two:
-            for (Map match : entry) {
+        for (ArrayList<Map> entry : map.values()) { // [{"red": 4, "blue": 3}, {"red": 1, "green": 2, "blue": 6}, {"green": 2}]
+            for (Map match : entry) { // {"red": 4, "blue": 3}
                 if (match.get("red") != null && (Integer) match.get("red") > max.get(0)) {
                     wrongoutput.put(map.keySet().iterator().next(), entry);
                     break one;
@@ -58,25 +55,25 @@ public class Main {
 
     public static Map<Integer, ArrayList<Map>> parse(String line) {
         Map<Integer, ArrayList<Map>> map = new HashMap<Integer, ArrayList<Map>>();
-        line = line.replace("Game ", "");
-        ArrayList<String> parts = new ArrayList<>(Arrays.asList(line.split(": ")));
-        ArrayList<String> matches = new ArrayList<>(Arrays.asList(parts.get(1).split("; ")));
+        line = line.replace("Game ", ""); // 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+        ArrayList<String> parts = new ArrayList<>(Arrays.asList(line.split(": "))); // "1", "3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"
+        ArrayList<String> matches = new ArrayList<>(Arrays.asList(parts.get(1).split("; "))); // ["1"], ["3 blue, 4 red", "1 red, 2 green, 6 blue", "2 green"]
         ArrayList<Map> matchMaps = new ArrayList<Map>();
-        for (String match : matches) {
+        for (String match : matches) { // "3 blue, 4 red"
             Map<String, Integer> matchMap = new HashMap<String, Integer>();
-            ArrayList<String> matchParts = new ArrayList<>(Arrays.asList(match.split(", ")));
-            for (String matchPart : matchParts) {
+            ArrayList<String> matchParts = new ArrayList<>(Arrays.asList(match.split(", "))); // ["3 blue", "4 red"]
+            for (String matchPart : matchParts) { // "3 blue"
                 if (matchPart.contains("red")) {
                     matchMap.put("red", Integer.parseInt(matchPart.replace(" red", "")));
                 } else if (matchPart.contains("blue")) {
-                    matchMap.put("blue", Integer.parseInt(matchPart.replace(" blue", "")));
+                    matchMap.put("blue", Integer.parseInt(matchPart.replace(" blue", ""))); // "3" -> 3
                 } else if (matchPart.contains("green")) {
                     matchMap.put("green", Integer.parseInt(matchPart.replace(" green", "")));
                 }
-            }
+            } // {"red": 4, "blue": 3}
             matchMaps.add(matchMap);
         }
-        map.put(Integer.parseInt(parts.get(0)), matchMaps);
+        map.put(Integer.parseInt(parts.get(0)), matchMaps); // {1: [{"red": 4, "blue": 3}, {"red": 1, "green": 2, "blue": 6}, {"green": 2}]}
 
         return map;
     }
